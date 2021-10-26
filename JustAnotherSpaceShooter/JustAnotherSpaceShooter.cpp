90a14a65760161b2a2945ea8c1b2aeb2b379fce1
Akash <akashshankar29@gmail.com>
2021-10-26 19:34:17 -0400

    An Empty GLFW Window

--- JustAnotherSpaceShooter/JustAnotherSpaceShooter.cpp
+++ JustAnotherSpaceShooter/JustAnotherSpaceShooter.cpp
@@ -1,6 +1,70 @@
+#include <windows.h>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 #include <iostream>
 
-int main()
+#include "InitShader.h"
+
+GLuint shader_program = -1;
+glm::vec2 window_dims = glm::vec2(1024, 768);
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-    std::cout << "Hello World!\n";
+    glViewport(0, 0, width, height);
+    window_dims = glm::vec2(width, height);
+}
+
+void keyboard(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (action == GLFW_PRESS)
+    {
+        switch (key)
+        {
+        case GLFW_KEY_ESCAPE:
+            glfwSetWindowShouldClose(window, GLFW_TRUE);
+            break;
+        }
+    }
+}
+
+void mouse_cursor(GLFWwindow* window, double x, double y)
+{
+}
+
+void mouse_button(GLFWwindow* window, int button, int action, int mods)
+{
+}
+
+int main(void)
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_SAMPLES, 1);
+
+    const char* window_name = "Just Another Space Shooter";
+    GLFWwindow* window = glfwCreateWindow(window_dims.x, window_dims.y, window_name, NULL, NULL);
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    glfwSetKeyCallback(window, keyboard);
+    glfwSetCursorPosCallback(window, mouse_cursor);
+    glfwSetMouseButtonCallback(window, mouse_button);
+    glEnable(GL_DEPTH_TEST);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        float time_sec = static_cast<float>(glfwGetTime());
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
