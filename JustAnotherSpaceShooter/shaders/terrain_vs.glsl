587ecd1276d93ebebf84a6d027fada1cd417be70
Akash <akashshankar29@gmail.com>
2021-11-09 19:02:26 -0500

    Directional lighting applied to terrain and spaceship.

--- JustAnotherSpaceShooter/shaders/terrain_vs.glsl
+++ JustAnotherSpaceShooter/shaders/terrain_vs.glsl
@@ -4,14 +4,17 @@ in vec3 pos_attrib;
 in vec2 tex_coord_attrib;
 in vec3 normal_attrib;
 
-out vec2 o_tex_coo_attrib;
-
 uniform mat4 terrainStart;
 uniform mat4 t_view;
 uniform mat4 t_projection;
 
+out vec3 var_pos_attrib;
+out vec3 var_normal_attrib;
+
 void main() 
 {
-    o_tex_coo_attrib = tex_coord_attrib;
+    var_pos_attrib = pos_attrib;
+    var_normal_attrib = mat3(transpose(inverse(terrainStart))) * normal_attrib;
+
     gl_Position = t_projection * t_view * terrainStart * vec4(pos_attrib, 1.0);
 }
